a83e33b860148e55df5504b18d8f4629bed8dc1e
Aman5685 <amanyadav5685@gmail.com>
2024-10-18 11:13:18 +0530

    Done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,74 @@
 // import React from 'react'
+import { useState } from "react";
+import pic from"../../public/mypic.jpg"
+import { AiOutlineMenu } from "react-icons/ai";
+import { IoClose } from "react-icons/io5";
 
 function Navbar() {
+    {/*menu->variale setMenu->Function*/}
+    const [menu,setmenu]=useState(false);
+    const navItems=[
+        {
+            id:1,
+            text:"Home"
+        },
+        {
+            id:2,
+            text:"About"
+        },
+        {
+            id:3,
+            text:"Portfolio"
+        },
+        {
+            id:4,
+            text:"Skills"
+        },
+        {
+            id:5,
+            text:"Contacts"
+        },
+    ]
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
-        <div>
-            <div>
-                <img src="" alt="" />
-                <h1>Ama <span>n</span>
-                <p>Web Developer</p>
-                </h1>
+      <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 h-16 shadow-md bg-red-900 fixed top-0 left-0 right-0">
+        <div className=" flex justify-between items-center   h-16">
+               {/*Right part of navbar*/}
+            <div className="flex space-x-2">
+                <img src={pic} className="h-12 w-12 rounded-full" alt="" />
+                <h1 className="font-semibold text-xl cursor-pointer">Ama
+                    <span className="text-green-500 text-2xl">n</span>
+                <p className="text-sm">Web Developer</p></h1>
             </div>
+            {/*Desktop Navbar*/}
             <div>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Portfolio</li>
-                    <li>Skills</li>
-                    <li>Contacts</li>
+                <ul className="hidden md:flex space-x-8">
+                    {
+                       navItems.map(({id,text})=>(
+                        <li className="hover:scale-105 hover:bg-slate-500 duration-200 cursor-pointer" key={id}>{text}</li>
+                       )) 
+                    }
                 </ul>
+                {/*Button function*/}
+                <div onClick={()=>setmenu(!menu)} className="md:hidden">{menu?<AiOutlineMenu size={24} />:<IoClose size={24} />}</div>
+
             </div>
         </div>
-        <div></div>
+        {/*Mobile Navbar*/}
+        {
+            menu && (
+        <div>
+            <ul className="md:hidden flex flex-col h-screen items-center justify-center space-y-3 text-xl">
+            {
+                       navItems.map(({id,text})=>(
+                        <li className="hover:scale-105 hover:bg-slate-500 duration-200 font-semibold cursor-pointer" key={id}>{text}</li>
+                       )) 
+                    } 
+            </ul>
+        </div>
+
+            )
+        }
       </div>
     </>
   )
