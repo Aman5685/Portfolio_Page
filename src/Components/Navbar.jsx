3003790ce8b1f2cc7d268198ffb8ab143289a03a
Aman5685 <amanyadav5685@gmail.com>
2024-10-18 09:23:26 +0530

    Done

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,30 @@
+// import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
+        <div>
+            <div>
+                <img src="" alt="" />
+                <h1>Ama <span>n</span>
+                <p>Web Developer</p>
+                </h1>
+            </div>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Portfolio</li>
+                    <li>Skills</li>
+                    <li>Contacts</li>
+                </ul>
+            </div>
+        </div>
+        <div></div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar;
