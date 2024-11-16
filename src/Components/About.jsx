d2fe10c8a16bacaf9d03229d49c22716476c915c
Aman Yadav <amanyadav5685@gmail.com>
2024-11-17 01:18:28 +0530

    Update About.jsx v2

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,39 +9,39 @@ function About() {
         Hello, I,am Aman, a passionate Web developer with a keen eye for MERN 
         Stack . With a background in IT, I strive to create impactful and
         visually stunning Software solution that leave a lasting impression.
-        My Future Goal is to get set the benchmark in the field of Dot Net[Developer].
+        My Future Goal is to get set the benchmark in the field of Dot Java-Developer.
       </p>
       <br />
       <h1 className="text-green-500 text-2xl font-semibold underline">Educational & Training</h1>
-      <p>[Bachelor of Computer Application],[Veer Bahadur Singh Purvanchal University],[2020-2023],
+      <p>Bachelor of Computer Application-Veer Bahadur Singh Purvanchal University-[2020-2023],
           <br />
-        [Master of Computer Application],[Lloyd Institute of Engineering and Technology],[2023-2025]
+        Master of Computer Application-Lloyd Institute of Engineering and Technology-[2023-2025]
       </p>
        <br />
        <h1 className="text-green-500 text-2xl underline font-semibold">Skills</h1>
-      <p>Proficient in[C & JAVA(For Problem Solving)],
-        Strong grasp of[Design Principles/Concepts],
+      <p>Proficient in:-C & JAVA(For Problem Solving)
+        Strong grasp of :-Design Principles/Concepts
         <br />
         Excellent problem-solving skills effective communicator and collaborator.
       </p>
       <br />
       <h1 className="text-green-500 text-2xl underline font-semibold">Software Tools & Technologies</h1>
        <p>
-        [HTML5],[CSS],[Tailwind-css],[Java-Script]
+        HTML5,CSS,Tailwind-css,Java-Script
         <br />
-        [React j.s],[Git-hub].
+        React j.s,Git-hub.
        </p>
        <br />
        <h1 className="text-green-500 text-2xl underline font-semibold">Achievements & Awards</h1>
         <p>
-        [Code blez in Hacker-rank],[Lloyd Institute of Engineering and Technology],[02/05/2024],[Participating Certificate].
+        Code blez in Hacker-rank,Lloyd Institute of Engineering and Technology,[02/05/2024]-Participating Certificate.
         <br />
-        [Web Development Fundamentals],[IBM Skills Build],[22/09/2024].[Complete all the course in the Web development fundamentals.]
+       Web Development Fundamentals,IBM Skills Build,[22/09/2024]-Complete all the course in the Web development fundamentals
         </p>
           <br />
           <h1 className="text-green-500 text-2xl underline font-semibold">Future Goal.</h1>
           <p>
-           My mission is to leverage my skills and creativity to deliver innovative[Full stack web Developer & Dot.Net] 
+           My mission is to leverage my skills and creativity to deliver innovative[Full stack web Developer & Java Developer] 
            solution that exceed client expectations and contribute positively to the digital landscape.
            I am committed to continuous learning and growth, always seeking new challenges and opportunities
            to expand my horizons.
